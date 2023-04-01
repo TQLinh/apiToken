41e8af97980a1f1514806d297cca60aebc594153
Linh <tranquoclinh13092001@gmail.com>
2023-04-01 22:30:09 +0700

    update file

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 routerApi(app);
-use.get("/", function (req, res) {
+app.get("/", function (req, res) {
   return res.status(200).send({ mes: "hi" });
 });
 //port
